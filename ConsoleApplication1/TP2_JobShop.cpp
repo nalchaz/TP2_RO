91de81ab01c089ea1f8d6c51336751332d17fd1d
Nalchaz <nahel.chazot@gmail.com>
2018-11-02 16:12:19 +0100

    Modification du fichier utilise

--- ConsoleApplication1/TP2_JobShop.cpp
+++ ConsoleApplication1/TP2_JobShop.cpp
@@ -10,7 +10,7 @@ using namespace System;
 
 int main() //args : array<System::String ^> ^args
 {
-	Tprobleme prob("la01.txt");
+	Tprobleme prob("ft06.txt");
 	//int vec[900] = { 5, 5, 4, 4, 5, 4, 0, 0, 4, 2, 5, 5, 1, 3, 1, 5, 0, 2, 2, 1, 1, 4, 2, 3, 2, 2, 1, 0, 4, 3, 3, 0, 3, 0, 1, 3 };
 	prob.lireFichier();
 	//prob.setVecteur(vec);
